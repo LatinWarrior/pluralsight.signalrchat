4fa8ebe116da36f72b33674fe7b3aacc3d1652ab
Luis Blanco 路易斯.布兰科 <luis_blanco@volusion.com>
2015-01-04 10:43:07 -0600

    Integration of SignalR and Angular is complete

--- Pluralsight.SignalRChat.Web/app/mainCtrl.js
+++ Pluralsight.SignalRChat.Web/app/mainCtrl.js
@@ -1,9 +1,16 @@
 ﻿angular.module('app').controller('mainCtrl', function($scope, chat) {
     $scope.messages = [];
 
-    $scope.sendMessage = function () {
-        chat.server.sendMessage('hi');
+    $scope.sendMessage = function() {
+        chat.server.sendMessage($scope.newMessage);
         // send scope.newMessage
         $scope.newMessage = "";
-    }
+    };
+
+    chat.client.newMessage = function onNewMessage(message) {
+        $scope.messages.push({ message: message });
+        // Kick off Angular's digest cycle manually so that Angular can update its bindings.
+        $scope.$apply();
+        console.log(message);
+    };
 });
