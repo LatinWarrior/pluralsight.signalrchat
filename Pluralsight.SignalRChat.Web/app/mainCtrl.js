5ccb83ff7460f938f460fb390538f3728273b331
Luis Blanco 路易斯.布兰科 <luis_blanco@volusion.com>
2015-01-04 11:42:07 -0600

    Added comments

--- Pluralsight.SignalRChat.Web/app/mainCtrl.js
+++ Pluralsight.SignalRChat.Web/app/mainCtrl.js
@@ -8,6 +8,8 @@
     };
 
     chat.client.newMessage = function onNewMessage(message) {
+        // Push an object onto the array of messages, not a string. 
+        // Otherwise, if there are duplicate messages, Angular won't be able to bind them to the ng-repeat.
         $scope.messages.push({ message: message });
         // Kick off Angular's digest cycle manually so that Angular can update its bindings.
         $scope.$apply();
